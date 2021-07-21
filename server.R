f0f25a481bb672b3a72d882f40b523949958bb51
TrijotSingh1989 <54065470+TrijotSingh1989@users.noreply.github.com>
2021-07-21 10:08:37 +0400

    FieldsLocCorrected
    Initially the location is hardcoded now we have made that dynamic

--- server.R
+++ server.R
@@ -2,6 +2,7 @@ library(pdftools)
 library(dplyr)
 library(tidyverse)
 library(stringi)
+library(tidyr)
 # Define server logic to read selected file ----
 server <- function(input, output) {
 
@@ -22,39 +23,64 @@ server <- function(input, output) {
 month <- 0
 year <- 0
 Claim_Value <- 0
+day <- 0
+#data1 = str_squish(data1)
+#data1 = str_replace_all(data1,",","")
+#data1 = strsplit(data1,split=" ")
+#data1 = unlist(data1)
+#seperate(data1)
+#x <- paste(data1, collapse = "")
+y = unlist(strsplit(data1, split = " "))
+k = stri_remove_empty(y, na_empty = FALSE)
+str_locate(k, '17a')
+t = unlist(gregexpr("17a", k))
+which(t == "True")
+datanumber = which(t > 0)
+datanumber = datanumber-1
+
 for(i in 1:12) {
   # i-th element of `u1` squared into `i`-th position of `usq`
-
-  test19<- data1[i+94] %>%
-  str_squish() %>%
-  str_replace_all(",","") %>%
-  strsplit(split=" ")
-  test20 <- test19[1] %>%
-    unlist()
-  month[i] <- test20[2]
-  year[i] <- test20[4]
-  Claim_Value[i] <- as.numeric(test20[5])
-
+  month[i] <- k[datanumber+(5*i-3)]
+  day[i] <- k[datanumber+(5*i-2)]
+  year[i] <- k[datanumber+(5*i-1)]
+  cl <- k[datanumber+5*i]
+  cl1 <- str_split(cl, "\n")
+  cl2 <- unlist(cl1)
+  cl3 <- cl2[1]
+  Claim_Value[i] <- as.numeric(gsub(",","",cl3))
 }
- Client_name_deri <- data1[3]%>%
-  str_squish() %>%
-  str_replace_all(",","") %>%
-  strsplit(split=" ")
 
-ClientName_deri1 <-Client_name_deri %>%
-  unlist()
+t1 = unlist(gregexpr("Policy", k))
+which(t1 == "True")
+datanumber1 = which(t1 > 0)
+datanumber1 = datanumber1-2
+k[datanumber1][1]
 
-ClientName <- c(ClientName_deri1[5],ClientName_deri1[6])
-ClientName <- paste0(ClientName_deri1[5],ClientName_deri1[6], collapse = NULL)
-
-client.data <- data.frame(ClientName, year, month,Claim_Value)
-
+t2 = unlist(gregexpr("Name", k))
+which(t2 == "True")
+datanumber2 = which(t2 > 0)
+datanumber2 = datanumber2+3
+k[datanumber2][1]
+ClientName <- ''
+seq(datanumber2[1], datanumber1[1])
+numberchar_clientName = datanumber1[1]-datanumber2[1]
+for(i in datanumber2[1]:datanumber1[1]) {
+  ClientName1 <- (k[i])
+  ClientName <- c(ClientName,paste(ClientName1, collapse = "," ))
+}
+ClientName_used1 = str_c(ClientName,collapse=',') 
+ClientName_used2 = str_c(ClientName_used1,collapse=',')
+ClientName_used3 = str_replace_all(ClientName_used2,",","")
+client.data <- data.frame(ClientName_used3, year, month,Claim_Value)
     #df1 <- c(data1[2],data1[2])
    #df1 <- c(data1[13384],data1[13564],data1[13546])
-    df1 <- data.frame(ClientName,month,year,Claim_Value)
+    df1 <- data.frame(ClientName_used3,month,year,Claim_Value)
    return(df1)
 
   })
 
 }
+
+
+
 
--- test.R
+++ test.R
@@ -1,17 +1,11 @@
-# Title     : TODO
-# Objective : TODO
-# Created by: TJIpadAccess
-# Created on: 06/11/20
-
-library(pdftools)
-library(dplyr)
-#setting the path where the PDF Files are saved
-file_vector <- list.files(path = getwd())
-file_vector %>% head()
-grepl(".pdf",file_vector)
-pdf_list <- file_vector[grepl(".pdf",file_vector)]
-
-#Axa Claims report for FlyDubai for testing purpose
-pdf_list[2]
-test <- pdf_text(pdf_list[2]) %>%
-  readr::read_lines()
+library(shiny)
+runApp(
+  appDir = getwd(),
+  port = getOption("shiny.port"),
+  launch.browser = getOption("shiny.launch.browser", interactive()),
+  host = getOption("shiny.host", "127.0.0.1"),
+  workerId = "",
+  quiet = FALSE,
+  display.mode = c("auto", "normal", "showcase"),
+  test.mode = getOption("shiny.testmode", FALSE)
+)
